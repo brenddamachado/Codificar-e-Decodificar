8f67555f9115becc55ef6951d0040cc49aeed28c
Brenda Machado <brenda16machado.bm@gmail.com>
2022-10-09 00:15:53 -0300

    js.editando

--- CodEDec.js
+++ CodEDec.js
@@ -1,7 +1,21 @@
-var seletor = document.querySelector("sele")
+var seletor = document.querySelector("#selecione")
 var addContainer = document.getElementById("valor")
 var texto = document.getElementById("txt")
 var txtResultado = document.getElementById("resul")
 var radioCode = document.getElementById("codi")
 var radioDecode = document.getElementById("decodi")
-var btnCodificar = document.getElementById("resul")
+var btnCodificar = document.getElementById("button")
+
+
+// Evento criado para quando o select mudar para o Cifra de Cesar a div escondida (passo) aparecer.
+seletor.addEventListener("change", function(event){
+    
+    if (event.target.value == "cifraCe") { 
+        addContainer.style = "display: block"; 
+
+    } else { 
+        
+        addContainer.style = "display: none"; 
+    }
+
+});
